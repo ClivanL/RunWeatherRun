1fb1283916308e5d7cfd69684158d11261063081
Clivan Lee <clivan.lee@outlook.com>
2022-06-06 22:45:42 +0800

    cleaned up codes

--- src/Pages/Tracks/Tracks.jsx
+++ src/Pages/Tracks/Tracks.jsx
@@ -1,17 +1,9 @@
-import GpxParser from 'gpxparser'
 import 'leaflet/dist/leaflet.css'
 import Navbar from '../../Components/Navbar'
-import runningroutes from '../../runningroutes'
 import BasicTabs from '../../Components/Tab'
 
 
 function Tracks(){
-
-    const gpxMaps=runningroutes.map((x)=>{
-        let gpx= new GpxParser();
-        gpx.parse(x.route);
-        return gpx.tracks[0].points.map(p => [p.lat, p.lon])   
-        })
 
     return(
         <>
