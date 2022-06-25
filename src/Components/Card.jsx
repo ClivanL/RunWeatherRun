8e817b742d2b7b1065e5000cc277d80d00ccc980
Clivan Lee <clivan.lee@outlook.com>
2022-06-25 12:41:14 +0800

    minor changes to improve appearance

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,18 +4,20 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import { borders } from '@mui/system'
 
 
 export default function ActionAreaCard({legend, text, images}) {
   // console.log("action area card",images);
   return (
-    <Card sx={{ width: 150 }}>
+    <Card sx={{ width: 150, borderColor:'primary.main', border: 1, borderRadius: '16px' }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="25%"
           image={images && images[text]}
           alt={text}
+          margin="auto"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
